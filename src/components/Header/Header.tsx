33bf2bd566bbeb28cc7770715390dbacfc1ce5f8
Nina Vasilieva <aninok92@gmail.com>
2024-05-30 14:39:25 +0300

    Move Login/Logout buttons to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,70 @@
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { logout } from '../../redux/userSlice';
+import DropdownMenu from '../DropdownMenu/DropdownMenu';
 
-const Header = () => {
+const Header: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector((state: RootState) => state.user.isLoggedIn);
+  const username = useSelector((state: RootState) => state.user.username);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/');
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <header className="bg-gray-800 py-4">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <h1 className="text-white text-xl font-bold">Ваше приложение</h1>
+          <h1 className="text-white text-xl font-bold">My quiz app</h1>
         </div>
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link to="/" className="text-white hover:text-gray-300">Главная</Link>
+              <Link to="/" className="text-white hover:text-gray-300">Home Page</Link>
             </li>
             <li>
-              <Link to="/quizzes" className="text-white hover:text-gray-300">Квизы</Link>
-            </li>
-            <li>
-              <Link to="/profile" className="text-white hover:text-gray-300">Профиль</Link>
-            </li>
-            <li>
-              <Link to="/admin" className="text-white hover:text-gray-300">Админ</Link>
+              <Link to="/quizzes" className="text-white hover:text-gray-300">Quizzes</Link>
             </li>
             <li>
               <Link to="/dashboard" className="text-white hover:text-gray-300">Dashboard</Link>
             </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">Войти</a>
-            </li>
+            {isAuthenticated ? (
+              <li className="relative">
+                <div
+                  className="text-white cursor-pointer"
+                  onClick={toggleDropdown}
+                >
+                  Welcome, {username}
+                </div>
+                {showDropdown && (
+                  <div onMouseLeave={() => setShowDropdown(false)}>
+                    <DropdownMenu>
+                      <button
+                        onClick={handleLogout}
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left"
+                      >
+                        Logout
+                      </button>
+                      {/* Add more items here */}
+                    </DropdownMenu>
+                  </div>
+                )}
+              </li>
+            ) : (
+              <li>
+                <Link to="/login" className="text-white hover:text-gray-300">Login</Link>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
---
+++ src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../redux/userSlice';
+
+const LoginPage: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState(''); // Имитация поля пароля
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+  
+    const handleLogin = () => {
+      dispatch(login(username));
+      navigate('/dashboard');
+    };
+  
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">Login</h1>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          className="mb-4 p-2 border"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="mb-4 p-2 border"
+        />
+        <button onClick={handleLogin} className="py-2 px-4 bg-blue-500 text-white">
+          Login
+        </button>
+      </div>
+    );
+  };
+  
+  export default LoginPage;
