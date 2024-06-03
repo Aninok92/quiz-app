77c562a09248edcbe6f701327a23bcf74b9336f8
Nina Vasilieva <aninok92@gmail.com>
2024-06-03 17:34:58 +0300

    add content in HomePage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,11 +1,19 @@
-import PageTitle from "../PageTitle/PageTitle";
-import Container from "../Container/Container";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageTitle from '../PageTitle/PageTitle';
+import Container from '../Container/Container';
 
-const HomePage = () => {
+const HomePage: React.FC = () => {
   return (
     <Container>
-      <PageTitle title={'Добро пожаловать на главную страницу!'} />
-      <p className="text-lg">Здесь вы можете добавить описание вашего приложения или предоставить ссылки на другие разделы.</p>
+      <PageTitle title="Welcome to QuizBrain" />
+      <p className="text-lg mb-4">
+        Здесь вы можете <Link to="/quizzes" className="text-blue-500 hover:underline">сыграть в наши квизы</Link> или 
+        <Link to="/create-quiz" className="text-blue-500 hover:underline ml-1">создать свой квиз</Link>.
+      </p>
+      <p className="text-lg">
+        Вы также можете <Link to="/dashboard" className="text-blue-500 hover:underline">посмотреть свою историю игр и результаты</Link>.
+      </p>
     </Container>
   );
 };
